00bb0285ecc36def5313114b696c85f7440ca259
Curstantine <curstantine@gmail.com>
2024-02-01 20:48:28 +0530

    chore: remove references to shitcn

--- app/uno.config.js
+++ app/uno.config.js
@@ -18,7 +18,7 @@ const importIconCollection = (name) => {
 
 export default defineConfig({
 	presets: [
-		presetUno({ dark: false }),
+		presetUno({ dark: "class" }),
 		presetTypography(),
 		presetIcons({
 			collections: {
@@ -43,6 +43,56 @@ export default defineConfig({
 		duration: {
 			standard: "300ms",
 			emphasized: "500ms",
+		},
+
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+
+		colors: {
+			border: "hsl(var(--border) / <alpha-value>)",
+			input: "hsl(var(--input) / <alpha-value>)",
+			ring: "hsl(var(--ring) / <alpha-value>)",
+			background: "hsl(var(--background) / <alpha-value>)",
+			foreground: "hsl(var(--foreground) / <alpha-value>)",
+			primary: {
+				DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+				foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
+			},
+			secondary: {
+				DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+				foreground: "hsl(var(--secondary-foreground) / <alpha-value>)",
+			},
+			destructive: {
+				DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+				foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
+			},
+			muted: {
+				DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+				foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
+			},
+			accent: {
+				DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+				foreground: "hsl(var(--accent-foreground) / <alpha-value>)",
+			},
+			popover: {
+				DEFAULT: "hsl(var(--popover) / <alpha-value>)",
+				foreground: "hsl(var(--popover-foreground) / <alpha-value>)",
+			},
+			card: {
+				DEFAULT: "hsl(var(--card) / <alpha-value>)",
+				foreground: "hsl(var(--card-foreground) / <alpha-value>)",
+			},
+		},
+
+		borderRadius: {
+			lg: "var(--radius)",
+			md: "calc(var(--radius) - 2px)",
+			sm: "calc(var(--radius) - 4px)",
 		},
 	},
 	shortcuts: {
