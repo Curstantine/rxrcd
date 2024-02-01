6812c6b99d0faae29db023353ef1709e1e1ddec4
Curstantine <curstantine@gmail.com>
2024-02-01 19:48:49 +0530

    chore: add window_shadows support for native compositor effects

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,29 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{thread, time::Duration};
+
+use tauri::WindowEvent;
+
+#[cfg(any(windows, target_os = "macos"))]
+use window_shadows::set_shadow;
+
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+
+            #[cfg(any(windows, target_os = "macos"))]
+            set_shadow(&window, true).unwrap();
+
+            Ok(())
+        })
+        .on_window_event(|e| {
+            // To alleviate the resize perf bugs mentioned in https://github.com/tauri-apps/tauri/issues/6322
+            if let WindowEvent::Resized(_) = e.event() {
+                thread::sleep(Duration::from_nanos(1));
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
