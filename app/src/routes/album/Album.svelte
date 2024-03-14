8f1c7ea71b6a76abde80e7f232e5b464a6039936
Curstantine <curstantine@proton.me>
2024-03-14 19:13:10 +0530

    chore: add album meta design

--- app/src/routes/album/Album.svelte
+++ app/src/routes/album/Album.svelte
@@ -11,4 +11,22 @@
 	$: $id = params.id;
 </script>
 
-<span>{$id}</span>
+<div class="flex flex-col">
+	<div class="h-64 flex items-center p-8 space-x-6">
+		<div class="aspect-square h-full rounded-lg bg-secondary">
+			<!-- <img src={$artist.picture_big} alt="{$artist.name}'s Avatar" class="w-full rounded-full" /> -->
+		</div>
+
+		<div class="flex flex-col">
+			<h1 class="select-text text-4xl text-foreground font-bold">Nurture</h1>
+			<h1 class="select-text text-xl text-primary">Porter Robinson</h1>
+			<div class="flex gap-2 pt-1 text-sm text-muted-foreground">
+				<span>14 tracks</span> ·
+				<span>58 minutes</span> ·
+				<span>{new Date("2021-04-24").toLocaleDateString()}</span> ·
+				<span>3,766 fans</span>
+			</div>
+		</div>
+	</div>
+	<span>{$id}</span>
+</div>
