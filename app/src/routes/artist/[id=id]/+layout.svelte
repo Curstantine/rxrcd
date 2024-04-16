d725e9aaa13c967f1948b0f8dad11572e4269c76
Curstantine <curstantine@proton.me>
2024-04-16 12:49:28 +0530

    chore: remove page hooks and move code to the layout

--- app/src/routes/artist/[id=id]/+layout.svelte
+++ app/src/routes/artist/[id=id]/+layout.svelte
@@ -1,19 +1,78 @@
 <script>
 	import { page } from "$app/stores";
 
-	import { tabs, extort_tab_state, style_tab, style_tab_bar } from "./page.hooks";
-
-	const { active_tab, on_tab_click } = extort_tab_state();
-
 	/** @type {import('./$types').PageData} */
 	export let data;
-	
-	const { artist: artistPromise } = data;
+
+	/**
+	 * @typedef {{ id: string, label: string, rel_type: import("$lib/types/deezer").AlbumRecordType | "discography" }} TabItem
+	 * @type {TabItem[]}
+	 */
+	const tabs = [
+		{ id: "tab_discography", rel_type: "discography", label: "Discography" },
+		{ id: "tab_albums", rel_type: "album", label: "Albums" },
+		{ id: "tab_eps", rel_type: "ep", label: "EPs" },
+		{ id: "tab_singles", rel_type: "single", label: "Singles" },
+		{ id: "tab_compilations", rel_type: "compilation", label: "Compilations" },
+	];
+
+	/**
+	 *  @param {HTMLDivElement} node
+	 */
+	function style_tab(node) {
+		/**
+		 * @param {TabItem} param0
+		 */
+		const update_styles = ({ id }) => {
+			const element = document.getElementById(id);
+			if (element === null) throw new Error(`Tab by #${id} doesn't exist`);
+
+			node.style.width = `${element.offsetWidth}px`;
+			node.style.transform = `translateX(${element.offsetLeft}px)`;
+		};
+
+		const un_sub = page.subscribe((o) => {
+			o.url;
+			update_styles();
+		});
+
+		return {
+			destroy: () => un_sub(),
+		};
+	}
+
+	/**
+	 *  @param {HTMLDivElement} node
+	 */
+	function style_tab_bar(node) {
+		const body = document.getElementsByTagName("body")[0];
+		let marked = false;
+
+		const styler = () => {
+			if (!marked && body.scrollTop >= node.offsetTop) {
+				node.style.setProperty("--un-bg-color-opacity", "0.95");
+				marked = true;
+				return;
+			}
+
+			if (marked && node.offsetTop > body.scrollTop) {
+				node.style.setProperty("--un-bg-color-opacity", "1");
+				marked = false;
+				return;
+			}
+		};
+
+		body.addEventListener("scroll", styler);
+
+		return {
+			destroy: () => body.removeEventListener("scroll", styler),
+		};
+	}
 </script>
 
 <div class="flex flex-col container">
 	<div class="h-64 flex items-center p-6">
-		{#await artistPromise}
+		{#await data.artist}
 			<div class="h-48 w-48 animate-pulse rounded-full bg-secondary use-transition-emphasized"></div>
 
 			<div class="flex flex-col gap-2 pl-6 transition-colors use-transition-emphasized">
@@ -41,13 +100,10 @@
 
 	<div use:style_tab_bar style="--un-bg-color-opacity: 1;" class="tab_bar">
 		{#each tabs as { id, label, rel_type }}
-			<a {id} class="tab" class:active={$active_tab.id === id} href="/artist/{}/{rel_type}">{label}</a>
+			<a {id} class="tab" class:active={$page.url.pathname.endsWith(rel_type)} href="./{rel_type}">{label}</a>
 		{/each}
 
-		<div
-			use:style_tab={active_tab}
-			class="absolute bottom-2 left-0 h-[2px] transform-gpu rounded-full bg-primary use-transition-standard"
-		/>
+		<div use:style_tab class="tab_bar_decoration" />
 	</div>
 
 	<slot />
@@ -65,5 +121,9 @@
 	.tab:hover,
 	.tab.active {
 		--at-apply: text-foreground;
+	}
+
+	.tab_bar_decoration {
+		--at-apply: absolute bottom-2 left-0 h-[2px] transform-gpu rounded-full bg-primary use-transition-standard;
 	}
 </style>
