6835dda4aafa1606c9e953c2fe9cb98ae97336cf
Curstantine <curstantine@proton.me>
2024-04-17 10:55:32 +0530

    chore: move /settings/account to sub route

---
+++ app/src/routes/settings/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import SettingsNavigationItem from "$lib/components/items/SettingsNavigationItem.svelte";
+</script>
+
+<main class="grid grid-cols-[16rem_1fr] w-full 2xl:container">
+	<div class="flex flex-col border-r-(1 border solid) px-2 pt-2">
+		<SettingsNavigationItem icon="i-symbols-person-outline-rounded" label="Account" />
+		<SettingsNavigationItem icon="i-symbols-palette-outline" label="Appearance" />
+		<SettingsNavigationItem icon="i-symbols-download-rounded" label="Downloads" />
+	</div>
+
+	<slot />
+</main>
---
+++ app/src/routes/settings/account/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import SettingsHeading from "$lib/components/SettingsHeading.svelte";
+</script>
+
+<article class="px-6 pt-4">
+	<SettingsHeading heading="Account" sub="Manage your authentication sessions and regions." />
+
+	<div class="auth-card">
+		<div class="grid-area-[image] h-24 w-24 self-center justify-self-center rounded-full bg-secondary"></div>
+
+		<div class="grid-area-[description] flex flex-col justify-center">
+			<span class="o">Not logged in</span>
+			<span class="text-sm text-muted-foreground">N/A</span>
+		</div>
+
+		<div class="grid-area-[actions]">
+			<button class="button-primary">Logout</button>
+		</div>
+	</div>
+</article>
+
+<style>
+	.auth-card {
+		--at-apply: grid border-border border-1 rounded-md p-4 gap-x-4 mt-3 max-w-lg;
+		grid-template-columns: auto 1fr 1fr;
+		grid-template-areas:
+			"image description description"
+			"image description description"
+			"image actions actions";
+	}
+</style>
