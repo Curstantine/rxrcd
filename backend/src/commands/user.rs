cc691bb5ecd92e82e2f9e21a24230365ddf1fe42
Curstantine <curstantine@proton.me>
2024-06-04 17:06:16 +0530

    fix(backend): get deezer client as a mutable reference in contexts where needed

--- backend/src/commands/user.rs
+++ backend/src/commands/user.rs
@@ -32,8 +32,8 @@ pub async fn get_auth_state<R: Runtime>(app_handle: AppHandle<R>) -> CommandResu
 #[tauri::command(rename_all = "snake_case")]
 #[tracing::instrument(skip(deezer_state), err(Debug))]
 pub async fn refresh_login(deezer_state: State<'_, DeezerClientState>) -> CommandResult<User> {
-	let deezer_guard = deezer_state.get().await;
-	let client = deezer_guard.as_ref().unwrap();
+	let mut deezer_guard = deezer_state.get().await;
+	let client = deezer_guard.as_mut().unwrap();
 	Ok(deezer::user::refresh_login(client).await.map(User::from)?)
 }
 
@@ -42,8 +42,8 @@ pub async fn refresh_login(deezer_state: State<'_, DeezerClientState>) -> Comman
 pub async fn login<R: Runtime>(app_handle: AppHandle<R>, data: UserAuthType) -> CommandResult<User> {
 	let login = {
 		let deezer_state = app_handle.state::<DeezerClientState>();
-		let deezer_guard = deezer_state.get().await;
-		let client = deezer_guard.as_ref().unwrap();
+		let mut deezer_guard = deezer_state.get().await;
+		let client = deezer_guard.as_mut().unwrap();
 
 		match &data {
 			UserAuthType::Arl { arl } => deezer::user::login_with_arl(client, arl).await.map(User::from)?,
@@ -62,8 +62,8 @@ pub async fn login<R: Runtime>(app_handle: AppHandle<R>, data: UserAuthType) -> 
 pub async fn logout<R: Runtime>(app_handle: AppHandle<R>) -> CommandResult<()> {
 	{
 		let deezer_state = app_handle.state::<DeezerClientState>();
-		let deezer_guard = deezer_state.get().await;
-		let client = deezer_guard.as_ref().unwrap();
+		let mut deezer_guard = deezer_state.get().await;
+		let client = deezer_guard.as_mut().unwrap();
 		deezer::user::logout(client).map_err(PassiveError::from)?;
 	}
 
@@ -84,12 +84,10 @@ pub async fn change_data_language(
 	deezer_state: State<'_, DeezerClientState>,
 	language: DataLanguage,
 ) -> CommandResult<()> {
-	let deezer_guard = deezer_state.get().await;
-	let client = deezer_guard.as_ref().unwrap();
+	let mut deezer_guard = deezer_state.get().await;
+	let client = deezer_guard.as_mut().unwrap();
 
-	let user_data_lock = client.get_user_data();
-	let user_data = user_data_lock.as_ref();
-	let user_language = user_data.map(|x| x.user.setting.global.language);
+	let user_language = client.user_data.as_ref().map(|x| x.user.setting.global.language);
 	let data_language = language.into_deezer_language(user_language);
 
 	client.set_language(Some(data_language));
