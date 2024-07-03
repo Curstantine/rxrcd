554ff33c9a57b4618415464bbc5c28e38e516061
Curstantine <curstantine@proton.me>
2024-07-03 21:07:54 +0530

    feat(backend): add anon user data structs in anon context, generalize state bearers to the UserVariant trait

--- packages/deezer/src/models/user.rs
+++ packages/deezer/src/models/user.rs
@@ -4,6 +4,11 @@ use super::{ajax::ResponseBody, Language};
 
 pub type GetUserDataResponse = ResponseBody<UserData>;
 
+pub trait UserVariant {
+	fn is_anon() -> bool;
+	fn api_token(&self) -> String;
+}
+
 #[derive(Debug, Clone, Deserialize)]
 #[serde(rename_all(deserialize = "SCREAMING_SNAKE_CASE"))]
 pub struct UserData {
@@ -55,4 +60,40 @@ pub struct UserSetting {
 pub struct UserSettingGlobal {
 	#[serde(with = "crate::serde::language_code")]
 	pub language: Language,
+}
+
+impl UserVariant for UserData {
+	fn is_anon() -> bool {
+		false
+	}
+
+	// TODO(Curstantine):
+	// We definitely can wrap the str in a Box, Rc or and Arc to prevent the unnecessary clones.
+	// Probably a good idea to look into this in the future when we are doing optimizations.
+	fn api_token(&self) -> String {
+		self.api_token.clone()
+	}
+}
+
+pub mod anon {
+	use serde::Deserialize;
+
+	#[derive(Debug, Clone, Deserialize)]
+	#[serde(rename_all(deserialize = "SCREAMING_SNAKE_CASE"))]
+	pub struct AnonUserData {
+		pub country: String,
+
+		#[serde(rename(deserialize = "checkForm"))]
+		pub api_token: String,
+	}
+
+	impl super::UserVariant for AnonUserData {
+		fn is_anon() -> bool {
+			true
+		}
+
+		fn api_token(&self) -> String {
+			self.api_token.clone()
+		}
+	}
 }
